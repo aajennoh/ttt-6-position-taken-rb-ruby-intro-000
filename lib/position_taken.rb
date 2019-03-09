35ca1221f4bad6b44aff623e036b32f4039cb5ee
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-09 05:09:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,10 @@
 # code your #position_taken? method here!
+require "pry"
 def position_taken?(board, index)
   if !(board[index] == "" || board[index] == " " || board[index] == nil)
     return true
   else
     return false
   end
-
-
-
 end
+binding.pry
